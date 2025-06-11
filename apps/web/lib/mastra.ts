8d7fceab0a46981c941bed194ff7e93b8d8d03e7
Paul Scanlon <pauliescanlon@gmail.com>
2025-06-11 15:28:01 -0700

    test: hardcode mastra lib baseURL

--- apps/web/lib/mastra.ts
+++ apps/web/lib/mastra.ts
@@ -1,5 +1,6 @@
 import { MastraClient } from "@mastra/client-js";
 
 export const mastraClient = new MastraClient({
-  baseUrl: process.env.NEXT_PUBLIC_MASTRA_API_URL || "http://localhost:4111/api"
+  baseUrl: "https://test-turbo-monorepo-api.vercel.app/"
+  // baseUrl: process.env.NEXT_PUBLIC_MASTRA_API_URL || "http://localhost:4111/"
 });
